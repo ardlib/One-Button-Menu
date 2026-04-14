9538cd8435f363e0e7654022cee619674ade04ea
boseji <boseji@users.noreply.github.com>
2026-04-14 19:56:16 +0530

    Change with new working program

---
+++ One-Button-Menu.ino
@@ -0,0 +1,30 @@
+// Using SAMD21 board
+
+#include "button.hpp"
+
+OBM btn(PA27);
+void buttonCallBack(bool state, uint32_t duration_ms) {
+  Serial.print(F("CB Current Button State: "));
+  Serial.println(state ? F("Pressed") : F("Released"));
+  Serial.print(F("CB Duration since Last Event (ms): "));
+  Serial.println(duration_ms);
+}
+void setup() {
+  delay(10000); // Startup delay to open Serial Console
+  
+  Serial.begin(115200);
+  
+  if (btn.begin()) {
+    Serial.println(F("Button Configured"));
+  }
+  btn.attach(buttonCallBack);
+  
+  Serial.println(F("Running!"));
+}
+
+void loop() {
+  // Check if the Buttonb is Detected
+  if (btn.detect()) {
+    Serial.println(F("Loop Detected Button Press"));
+  }
+}
