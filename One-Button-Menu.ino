a4a8256e72d60982fc85169bc3d58cf7d873ce69
boseji <boseji@users.noreply.github.com>
2026-04-14 21:18:31 +0530

    Refactor detection 1

--- One-Button-Menu.ino
+++ One-Button-Menu.ino
@@ -3,28 +3,31 @@
 #include "button.hpp"
 
 OBM btn(PA27);
-void buttonCallBack(int state, uint32_t duration_ms) {
-  Serial.print(F("CB Current Button State: "));
-  Serial.println(state ? F("Pressed") : F("Released"));
-  Serial.print(F("CB Duration since Last Event (ms): "));
-  Serial.println(duration_ms);
+void buttonCallBack(byte state, uint32_t duration_ms)
+{
+    Serial.print(F("CB Current Button State: "));
+    Serial.println(state ? F("Pressed") : F("Released"));
+    Serial.print(F("CB Duration since Last Event (ms): "));
+    Serial.println(duration_ms);
 }
-void setup() {
-  delay(10000); // Startup delay to open Serial Console
-  
-  Serial.begin(115200);
-  
-  if (btn.begin()) {
-    Serial.println(F("Button Configured"));
-  }
-  btn.attach(buttonCallBack);
-  
-  Serial.println(F("Running!"));
+void setup()
+{
+    delay(10000); // Startup delay to open Serial Console
+
+    Serial.begin(115200);
+
+    if (btn.begin()) {
+        Serial.println(F("Button Configured"));
+    }
+    btn.attach(buttonCallBack);
+
+    Serial.println(F("Running!"));
 }
 
-void loop() {
-  // Check if the Buttonb is Detected
-  if (btn.detect()) {
-    Serial.println(F("Loop Detected Button Press"));
-  }
+void loop()
+{
+    // Check if the Buttonb is Detected
+    if (btn.detect()) {
+        Serial.println(F("Loop Detected Button Press"));
+    }
 }
--- button.hpp
+++ button.hpp
@@ -6,156 +6,175 @@
 
 // State Callback
 // First parameter current state and change Since
-typedef void (*CB)(bool state, uint32_t since);
+typedef void (*CB)(byte state, uint32_t since);
 
 // One Button Menu Class
 class OBM {
 
 private:
-  bool isPressed;
-  bool isDetected;
-  uint32_t ts;
-  byte pin;
-  uint8_t mode;
-  CB func;
-  bool isConfigured;
-  uint32_t debounce_ms;
+    bool isPressed;
+    bool isDetected;
+    uint32_t ts;
+    byte pin;
+    uint8_t mode;
+    CB func;
+    bool isConfigured;
+    uint32_t debounce_ms;
 
 private:
-  // Check if the Valid Button was initialized
-  bool _valid() {
-    if (isConfigured && pin != 255 && debounce_ms != 0 && mode != 255) {
-      return true;
+    // Check if the Valid Button was initialized
+    bool _valid()
+    {
+        if (isConfigured && pin != 255 && debounce_ms != 0 && mode != 255) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-public:
-  // Blank Constructor for Default Cases
-  OBM() : OBM(255, 255, NULL, 0) {}
-
-  // Direct Specification type Constructor
-  OBM(byte pin, uint8_t mode = INPUT_PULLUP) : OBM(pin, mode, NULL, 0) {}
-
-  // Full Spec Constructor
-  OBM(byte pn, uint8_t md, CB fn, uint32_t debounceTime_ms) {
-    // Setup the Button
-    isPressed = false;
-    isDetected = false;
-    ts = 0;
-    func = fn;
-    pin = pn;
-    mode = md;
-    isConfigured = false;
-    debounce_ms = debounceTime_ms;
-  }
-
-  // Setup the Pins
-  void setup(byte pin, uint8_t mode = INPUT_PULLUP) {
-    pin = pin;
-    mode = mode;
-  }
-
-  // Setup Call back function
-  void attach(CB fn) { func = fn; }
-
-  // Configure the Loop and Debounce
-  bool begin(uint32_t debounceTime_ms = DEB_TIME) {
-    if (pin == 255)
-      return false;
-    // Reset the Button Configuration
-    pinMode(pin, mode);
-    // Set the Debounce time
-    if (debounce_ms < debounceTime_ms) {
-      debounce_ms = debounceTime_ms;
-    }
-    // Set the Configuration is Done
-    isConfigured = true;
-    // Success
-    return true;
-  }
-
-  // Loop Check For Single Press
-  bool detect(PinStatus detlevel = LOW) {
-    if (!_valid())
-      return false;
-    // Get the current time
-    uint32_t ct = millis();
-    // Read the button one more time
-    bool state = (digitalRead(pin) == detlevel);
-
-    // Check if the Button is in Pressed state
-    if (isPressed) {
-
-      // if the Required time has passed
-      if (ts != 0 && ((ct - ts) >= debounce_ms)) {
+    // Check the things in Pressed State
+    bool _checkInPressed(bool state, uint32_t ct)
+    {
+        // if the Required time has passed
+        if (ts == 0 || ((ct - ts) < debounce_ms)) {
+            return false;
+        }
 
         // If the button is indeed in the Level Detected / Pressed state
         if (state) {
-          isPressed = false;
-          isDetected = true; // Set the Detected state
-          ts = ct;           // Set the Time when it was detected
+            isPressed = false;
+            isDetected = true; // Set the Detected state
+            ts = ct; // Set the Time when it was detected
 #ifdef DEBUG
-          Serial.println(F("Button Press Detected"));
+            Serial.println(F("Button Press Detected"));
 #endif
-          // Callback if available - Confirmed Press Event
-          if (func != NULL) {
-            func(true, ct);
-          }
+            // Callback if available - Confirmed Press Event
+            if (func != NULL) {
+                func(true, ct);
+            }
 
-          // Return the Button Press Confirmed
-          return true;
-          // End of Pressed State Detection
-        } else {
-          // Button was released prematurely
-          isPressed = false;
-          isDetected = false; // Set the un-detected state
-          ts = 0;             // Clear the timeout
-#ifdef DEBUG
-          Serial.println(F("Button Released prematurely"));
-#endif
-        } // End of Level Detection for Button
+            // Return the Button Press Confirmed
+            return true;
+        }
 
-      } // End of Time check for button
-
-    } else if (isDetected) {
-      // If the button is in the Detected State
-
-      // Check if still pressed or not
-      if (!state) {
-        // Button was released
-        isDetected = false; // Set the un-detected state
+        // Otherwise Button was released prematurely
         isPressed = false;
-        uint32_t diff = ct - ts;
+        isDetected = false; // Set the un-detected state
         ts = 0; // Clear the timeout
 #ifdef DEBUG
-        Serial.println(F("Button Released Correctly"));
+        Serial.println(F("Button Released prematurely"));
 #endif
-        // Callback if available - Released Event
-        if (func != NULL) {
-          func(false, diff);
-        }
-        // End of Released State Processing
-      }
-      // Other wise we wait till the button is released
+        return false;
+    }
 
-    } else {
+public:
+    // Blank Constructor for Default Cases
+    OBM()
+        : OBM(255, 255, NULL, 0)
+    {
+    }
 
-      // If both above cases are not true and Button was not touched earlier
-      if (state) {
-        // Button is indeed pressed
+    // Direct Specification type Constructor
+    OBM(byte pin, uint8_t mode = INPUT_PULLUP)
+        : OBM(pin, mode, NULL, 0)
+    {
+    }
+
+    // Full Spec Constructor
+    OBM(byte pn, uint8_t md, CB fn, uint32_t debounceTime_ms)
+    {
+        // Setup the Button
+        isPressed = false;
         isDetected = false;
-        isPressed = true; // Pressed State for De-bounce
-        ts = ct;          // Timeout updated
+        ts = 0;
+        func = fn;
+        pin = pn;
+        mode = md;
+        isConfigured = false;
+        debounce_ms = debounceTime_ms;
+    }
+
+    // Setup the Pins
+    void setup(byte pin, uint8_t mode = INPUT_PULLUP)
+    {
+        pin = pin;
+        mode = mode;
+    }
+
+    // Setup Call back function
+    void attach(CB fn) { func = fn; }
+
+    // Configure the Loop and Debounce
+    bool begin(uint32_t debounceTime_ms = DEB_TIME)
+    {
+        if (pin == 255)
+            return false;
+        // Reset the Button Configuration
+        pinMode(pin, mode);
+        // Set the Debounce time
+        if (debounce_ms < debounceTime_ms) {
+            debounce_ms = debounceTime_ms;
+        }
+        // Set the Configuration is Done
+        isConfigured = true;
+        // Success
+        return true;
+    }
+
+    // Loop Check For Single Press
+    bool detect(PinStatus detlevel = LOW)
+    {
+        if (!_valid())
+            return false;
+        // Get the current time
+        uint32_t ct = millis();
+        // Read the button one more time
+        bool state = (digitalRead(pin) == detlevel);
+
+        // Check if the Button is in Pressed state
+        if (isPressed) {
+          return _checkInPressed(state, ct);
+        }
+
+        // if In detected State - Button was Pressed
+        // We are waiting for its release
+        if (isDetected) {
+            // If the button is in the Detected State
+
+            // Check if still pressed or not
+            if (!state) {
+                // Button was released
+                isDetected = false; // Set the un-detected state
+                isPressed = false;
+                uint32_t diff = ct - ts;
+                ts = 0; // Clear the timeout
 #ifdef DEBUG
-        Serial.println(F("Button Just Pressed"));
+                Serial.println(F("Button Released Correctly"));
 #endif
-      }
-      // Other wise the button was not pressed and we have nothing to do
+                // Callback if available - Released Event
+                if (func != NULL) {
+                    func(false, diff);
+                }
+                // End of Released State Processing
+            }
+            // Other wise we wait till the button is released
 
-    } // End of Button Checks
+        } else {
 
-    // Return the normally Non-Detected State
-    return false;
-  }
+            // If both above cases are not true and Button was not touched earlier
+            if (state) {
+                // Button is indeed pressed
+                isDetected = false;
+                isPressed = true; // Pressed State for De-bounce
+                ts = ct; // Timeout updated
+#ifdef DEBUG
+                Serial.println(F("Button Just Pressed"));
+#endif
+            }
+            // Other wise the button was not pressed and we have nothing to do
+
+        } // End of Button Checks
+
+        // Return the normally Non-Detected State
+        return false;
+    }
 };
